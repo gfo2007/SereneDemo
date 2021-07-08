9c3b5be8e5c94353d94c6af90d5ec6f21a1bceae
gfo2007 <gfo.2007@gmail.com>
2021-07-08 07:08:25 +0000

    Commit appsettings connection strings and database migrations

---
+++ SereneDemo/SereneDemo.Web/Migrations/DefaultDB/DefaultDB_20170430_134800_MultiTenant.cs
@@ -0,0 +1,50 @@
+﻿using FluentMigrator;
+using SereneDemo.Migrations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SereneDemo.Web.Migrations.DefaultDB
+{
+    [FluentMigrator.Migration(20170430134800)]
+    public class DefaultDB_20170430_134800_MultiTenant : AutoReversingMigration
+    {
+        public override void Up()
+        {
+            this.CreateTableWithId32("Tenants", "TenantId", s => s
+                .WithColumn("TenantName").AsString(100)
+                    .NotNullable());
+
+            Insert.IntoTable("Tenants")
+                .Row(new
+                {
+                    TenantName = "Primary Tenant"
+                });
+
+            Insert.IntoTable("Tenants")
+                .Row(new
+                {
+                    TenantName = "Second Tenant"
+                });
+
+            Insert.IntoTable("Tenants")
+                .Row(new
+                {
+                    TenantName = "Third Tenant"
+                });
+
+            Alter.Table("Users")
+                .AddColumn("TenantId").AsInt32()
+                    .NotNullable().WithDefaultValue(1);
+
+            Alter.Table("Roles")
+                .AddColumn("TenantId").AsInt32()
+                    .NotNullable().WithDefaultValue(1);
+
+            Alter.Table("Languages")
+                .AddColumn("TenantId").AsInt32()
+                    .NotNullable().WithDefaultValue(1);
+        }
+    }
+}
---
+++ SereneDemo/SereneDemo.Web/Migrations/NorthwindDB/NorthwindDB_20160110_093500_MultiTenant.cs
@@ -0,0 +1,51 @@
+﻿using FluentMigrator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SereneDemo.Web.Migrations.NorthwindDB
+{
+    [Migration(20170430194100)]
+    public class NorthwindDB_20170430_194100_MultiTenant : AutoReversingMigration
+    {
+        public override void Up()
+        {
+            Alter.Table("Employees")
+                .AddColumn("TenantId").AsInt32()
+                    .NotNullable().WithDefaultValue(1);
+
+            Alter.Table("Categories")
+                .AddColumn("TenantId").AsInt32()
+                    .NotNullable().WithDefaultValue(1);
+
+            Alter.Table("Customers")
+                .AddColumn("TenantId").AsInt32()
+                    .NotNullable().WithDefaultValue(1);
+
+            Alter.Table("Shippers")
+                .AddColumn("TenantId").AsInt32()
+                    .NotNullable().WithDefaultValue(1);
+
+            Alter.Table("Suppliers")
+                .AddColumn("TenantId").AsInt32()
+                    .NotNullable().WithDefaultValue(1);
+
+            Alter.Table("Orders")
+                .AddColumn("TenantId").AsInt32()
+                    .NotNullable().WithDefaultValue(1);
+
+            Alter.Table("Products")
+                .AddColumn("TenantId").AsInt32()
+                    .NotNullable().WithDefaultValue(1);
+
+            Alter.Table("Region")
+                .AddColumn("TenantId").AsInt32()
+                    .NotNullable().WithDefaultValue(1);
+
+            Alter.Table("Territories")
+                .AddColumn("TenantId").AsInt32()
+                    .NotNullable().WithDefaultValue(1);
+        }
+    }
+}
